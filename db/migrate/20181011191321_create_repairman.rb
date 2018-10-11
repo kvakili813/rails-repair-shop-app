e58e795fef4ef40a89b26382a919d7e1b36e9a0b
Kayvan <kvakili813@gmail.com>
2018-10-11 15:20:17 -0400

    creates db migration folder

---
+++ db/migrate/20181011191321_create_repairman.rb
@@ -0,0 +1,10 @@
+class CreateRepairman < ActiveRecord::Migration[5.2]
+  def change
+    create_table :repairmen do |t|
+      t.string :name
+      t.integer :uid, limit:10
+
+      t.timestamps null: false
+    end
+  end
+end
