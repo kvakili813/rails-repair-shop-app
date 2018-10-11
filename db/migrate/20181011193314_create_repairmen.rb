e5c941c78c4b162e37fd3c85d362963c24460cef
Kayvan <kvakili813@gmail.com>
2018-10-11 15:37:20 -0400

    creates model and migration

---
+++ db/migrate/20181011193314_create_repairmen.rb
@@ -0,0 +1,10 @@
+class CreateRepairmen < ActiveRecord::Migration[5.2]
+  def change
+    create_table :repairmen do |t|
+      t.string :name
+      t.integer :uid, limit: 10
+      
+      t.timestamps
+    end
+  end
+end
