cd66664ab8b465fe1d75e4272c0d3ac4456c1160
Kayvan <kvakili813@gmail.com>
2018-10-15 14:56:21 -0400

    updates tickets controller

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,2 +1,9 @@
 class TicketsController < ApplicationController
+  def complete
+    params[:ids].each do |id|
+      ticket = Ticket.find_by(id: id)
+      ticket.update(status: 1)
+    end
+    redirect_to '/'
+  end
 end
