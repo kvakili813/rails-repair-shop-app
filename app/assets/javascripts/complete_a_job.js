f608b27f0f797012304edbf560779af9cd842e82
Kayvan <kvakili813@gmail.com>
2018-11-09 01:32:35 -0500

    Creates completejob function

--- app/assets/javascripts/complete_a_job.js
+++ app/assets/javascripts/complete_a_job.js
@@ -1,3 +1,14 @@
 $(function(){
   completeJob();
 });
+
+function completeJob(){
+  $(document).on('click', '#mark-complete', function(event){
+    event.preventDefault();
+    var ids = getCheckedBoxes();
+    var pending_url = '/jobs?state=pending';
+    var completed_url = '/jobs?state=completed';
+    if ($('#pending-header').text() === "Your Pending Jobs:"){
+      pending_url = '/jobs?state=pending&scoped=true';
+      completed_url ='/jobs?state=completed&scoped=true';
+    }
