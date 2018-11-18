407577a405a24ef4f4b4977e93f97f239adbb42d
Kayvan <kvakili813@gmail.com>
2018-11-17 19:44:51 -0500

    Fixed Type error

--- app/assets/javascripts/create_jobs.js
+++ app/assets/javascripts/create_jobs.js
@@ -5,3 +5,23 @@ $(function(){
 function createJob(){
   $(document).on('click', '#create-job', function(event){
     event.preventDefault();
+    var url = window.location.pathname.slice(0, -3);
+    // var data = $('form').serialize();
+    // $.post('url', data()
+    // ).done(function(data){
+    $.post(url, $('form').serialize()
+     ).done(function(data){
+        var job = data['id'];
+        var str = '<li>Repairman: ' + data['repairman']['name'] + '</li>';
+        str += '<li>Customer: ' + data['customer']['name'] + '</li>';
+        str += '<li>Tickets:<ul>';
+        data['tickets'].forEach(function(order){
+          str += '<li>Description: ' + order['description'] + '</li>';
+          str += '<li>Price: $' + order['price'] + '</li>';
+        });
+        str += '</ul></li>';
+        $('#new-ajax-jobs').append(str);
+        $('input[type=text]').val('');
+      });
+  });
+}
